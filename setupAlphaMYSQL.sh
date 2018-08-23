3ba7b0bd3835f73e88a9b4e6f9163dd0083c1e40
wvbirder <32797703+wvbirder@users.noreply.github.com>
2018-08-23 13:54:01 -0400

    Update setupAlphaMYSQL.sh

--- setupAlphaMYSQL.sh
+++ setupAlphaMYSQL.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-mysql -uroot -p`grep 'temporary password' /var/log/mysqld.log | awk -e '{ print $11 }'` < changeRootPWD.sql
+mysql -uroot -p`grep 'temporary password' /var/log/mysqld.log | awk -e '{ print $11 }'` --connect-expired-password
 mysql -uroot -pAlpha2018_ < createUserAlpha.sql
 mysql -ualpha -pAlpha2018_ < createProducts.sql
