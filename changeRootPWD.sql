f3fe7798fe4bd7b937c9e687a98ba5557003f085
wvbirder <32797703+wvbirder@users.noreply.github.com>
2018-08-23 15:51:19 -0400

    Update changeRootPWD.sql

--- changeRootPWD.sql
+++ changeRootPWD.sql
@@ -1,2 +1,2 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'Alpha2018_';
+UPDATE mysql.user SET authentication_string = PASSWORD('Alpha2018_'), password_expired = 'N' WHERE User = 'root' AND Host = 'localhost';
 exit;
